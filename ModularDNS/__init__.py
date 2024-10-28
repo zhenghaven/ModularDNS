078aaea77d9fc64f9448cb188506193f1b4d5750
Haofan Zheng <hzheng6@ucsc.edu>
2024-10-28 01:34:55 -0700

    Fixed obj resource leak in

--- ModularDNS/__init__.py
+++ ModularDNS/__init__.py
@@ -10,7 +10,7 @@
 
 PACKAGE_INFO = {
 	'name': 'ModularDNS',
-	'version': '0.1.5',
+	'version': '0.1.6',
 	'author': 'Haofan Zheng',
 	'url': 'https://github.com/zhenghaven/ModularDNS',
 	'license': 'MIT',
