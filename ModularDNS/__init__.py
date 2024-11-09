704584eacdc4279b63a8378ad89feec88e70de5f
Haofan Zheng <hzheng6@ucsc.edu>
2024-11-08 22:51:23 -0800

    fixed QtAnsLog that won't call qtHandler if the question doesn't match

--- ModularDNS/__init__.py
+++ ModularDNS/__init__.py
@@ -10,7 +10,7 @@
 
 PACKAGE_INFO = {
 	'name': 'ModularDNS',
-	'version': '0.2.2',
+	'version': '0.2.3',
 	'author': 'Haofan Zheng',
 	'url': 'https://github.com/zhenghaven/ModularDNS',
 	'license': 'MIT',
